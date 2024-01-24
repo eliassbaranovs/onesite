c013ad2905c269a6ee71b33252adb802ddd94a5e
eliassbaranovs <82933682+eliassbaranovs@users.noreply.github.com>
2024-01-24 20:31:07 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Features from "./components/Features";
+import Form from "./components/Form";
+import Hero from "./components/Hero";
+import Partners from "./components/Partners";
+import Testimonial from "./components/Testimonial";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Partners />
+      <Testimonial />
+      <Form />
+    </>
+  );
+}
+
+export default App;
